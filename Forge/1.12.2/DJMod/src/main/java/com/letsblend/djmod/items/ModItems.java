99b0c29208edc672dc82c27e3944e7d9b66f274a
LetsBlend <david.toarniczky@gmail.com>
2023-08-25 11:22:41 +0200

    Some build updates and patches

---
+++ Forge/1.12.2/DJMod/src/main/java/com/letsblend/djmod/items/ModItems.java
@@ -0,0 +1,7 @@
+package com.letsblend.djmod.items;
+
+import net.minecraft.item.Item;
+
+public class ModItems {
+    public static Item LSD = new Item();
+}
---
+++ Forge/1.12.2/DJMod/src/main/java/com/letsblend/djmod/items/RegistryHandler.java
@@ -0,0 +1,14 @@
+package com.letsblend.djmod.items;
+
+import net.minecraft.item.Item;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+@Mod.EventBusSubscriber
+public class RegistryHandler {
+    @SubscribeEvent
+    public static void onItemRegister(RegistryEvent.Register<Item> event){
+        //event.getRegistry().register(ModItems.LSD);
+    }
+}
