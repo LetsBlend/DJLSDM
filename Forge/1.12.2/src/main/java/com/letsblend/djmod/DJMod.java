981f31fc6006e57e79dbacc7c004203bf36e392c
LetsBlend <david.toarniczky@gmail.com>
2023-08-25 10:06:04 +0200

    Basic first mod setup, Minecraft launching with the mod!

--- Forge/1.12.2/src/main/java/com/letsblend/djmod/DJMod.java
+++ Forge/1.12.2/src/main/java/com/letsblend/djmod/DJMod.java
@@ -7,11 +7,11 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import org.apache.logging.log4j.Logger;
 
-@Mod(modid = JDMod.MODID, name = JDMod.NAME, version = JDMod.VERSION)
-public class JDMod
+@Mod(modid = DJMod.MODID, name = DJMod.NAME, version = DJMod.VERSION)
+public class DJMod
 {
     public static final String MODID = "djmod";
-    public static final String NAME = "Example Mod";
+    public static final String NAME = "DJMod";
     public static final String VERSION = "1.0";
 
     private static Logger logger;
